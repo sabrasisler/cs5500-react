0a529c3bb1f811ec77878a6e3727ee89639f93c1
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-03-25 18:12:08 -0400

    deleted TODO app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TodoApp from "./todos/TodoApp";
 
 ReactDOM.render(
   <React.StrictMode>
