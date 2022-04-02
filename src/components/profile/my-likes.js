4f94554c46e57a38dcedf1e4942692ffb74eef45
Sabra <52722031+sabrasisler@users.noreply.github.com>
2022-04-02 16:19:26 -0400

    js doc for the likes screen and the profile

--- src/components/profile/my-likes.js
+++ src/components/profile/my-likes.js
@@ -1,3 +1,8 @@
+/**
+ * @file Renders the Likes screen in a User's profile, which displays all the 
+ * Tuits a user likes. Used the professor's A4 source code.
+ */
+
 import Tuits from "../tuits";
 import * as service from "../../services/likes-service";
 import {useEffect, useState} from "react";
